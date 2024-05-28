3eda6abf09f0e4f831117e7676e3d68f02fc0873
Lucia <lzgalarza@gmail.com>
2024-05-28 16:31:51 +0100

    Example of a triple branched if else

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const response = prompt("Books vs movies")
 if (response === "books") {
     console.log("BOOKS")
-} else {
+} else if (response === "movies") {
     console.log("FILMS")
+} else {
+    console.log("NO IDEA")
 }
